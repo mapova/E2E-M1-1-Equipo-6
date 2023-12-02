738b08931e1c3d9b03c7b5a8ddaf0a03b27c3b54
cmarty02 <cristianvmarty@gmail.com>
2023-12-02 12:31:37 +0100

    update

--- script.py
+++ script.py
@@ -1,66 +1,31 @@
-#Para llamar a la API primero importamos las bibliotecas requests y JSON para realizar solicitudes HTPP y analizar la respuesta json
-# respectivamente. Luego, definimos la URL de la API y hacemos una solicitud GET utilizando la función requests.get(). 
-# La respuesta se almacena en la variable response, y luego usamos la función json.loads() para analizar el contenido de la respuesta 
-# en formato JSON. Finalmente, imprimimos el valor del chiste aleatorio utilizando la clave value del diccionario data.
-# Para guardar el texto que hago del API pongo:
-
-import requests 
-import json
-import pandas as pd
+import requests
 import pymongo
 
-url= 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?limit=20'
+# Conexión a MongoDB
+client = pymongo.MongoClient("mongodb://admin:admin@localhost:27017/")
+db = client['valenbici']
+collection = db['sample_table']
 
-response = requests.get(url)
+# API URL para obtener datos de muestra
+api_url = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?limit=20'
 
-if response.status_code == 200:  # Verificar si la solicitud fue exitosa (código 200)
-    data = response.json()  # Obtener los datos en formato JSON
-    # Trabajar con los datos obtenidos de la API
-    print(data)
-    results = data['results'] 
-    # Establecer conexión con la base de datos MongoDB
-    client = pymongo.MongoClient('mongodb://localhost:27017/')  # URL de conexión de tu base de datos
-    db = client['nombre_de_tu_base_de_datos']  # Selecciona la base de datos
-    collection = db['nombre_de_tu_coleccion']  # Selecciona la colección
-    
-    # Insertar los datos en la colección de MongoDB
-    collection.insert_many(results)
-    print("Datos insertados en MongoDB correctamente.")
-else:
-    print('Hubo un problema con la solicitud. Código de estado:', response.status_code)
+try:
+    # Realizar solicitud HTTP a la API
+    response = requests.get(api_url)
 
+    # Verificar si la solicitud fue exitosa (código 200)
+    if response.status_code == 200:
+        # Obtener datos JSON de la respuesta
+        data = response.json()
 
+        # Actualizar la colección en MongoDB
+        collection.insert_many(data['results'])
 
+        print("Datos insertados correctamente.")
 
+    else:
+        print(f"Error al obtener datos: {response.status_code}")
 
+except Exception as e:
+    print(f"Error: {e}")
 
-
-
-
-
-
-
-'''
-   # df = pd.DataFrame([result['address','number','open','available','free','total','ticket','update_at','geo_schape','geo_point_2d'] for result in results])  # Extraer los datos relevantes de lso registros y convertir los datos a DataFrame
-    df = pd.DataFrame(data)
-   '''
-
-
-   #Configurar Pandas para mostrar todas las columnas y filas sin truncar
-'''
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_rows', None)
-    # Mostrar todas las columnas del DataFrame
-    print("Todas las columnas del DataFrame:")
-    print(df)
-    print("DataFrame creado con éxito:") #muestro el DataFrame
-    print(df.head())  # Mostrar las primeras filas del DataFrame
-
-else:
-    print('Hubo un problema con la solicitud. Código de estado:', response.status_code)
-
-'''
-
-
-
- 
