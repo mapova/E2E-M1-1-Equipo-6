229f319cc5a50b4f50335a489fb37e235b2a976e
cmarty02 <cristianvmarty@gmail.com>
2023-12-02 19:23:52 +0100

    FINAL!

---
+++ .bash_history
@@ -0,0 +1,8 @@
+python /home/jovyan/script_2.py
+exit
+python /home/jovyan/script_2.py
+& D:/Anaconda/python.exe d:/Dropbox/Facu/EDEM/GitHub/GitHub_Repositorios/E2E-M1-1-Equipo-6/script_2.py
+exit
+[200~python /home/jovyan/script_2.py~
+python /home/jovyan/script_2.py
+exit
--- script_2.py
+++ script_2.py
@@ -1,18 +1,22 @@
 import requests
 import pymongo
-import schedule
 import time
 
+#time.sleep(10)
+
 # Conexión a MongoDB
-client = pymongo.MongoClient("mongodb://admin:admin@localhost:27017/")
+client = pymongo.MongoClient("mongodb://admin:admin@mongo:27017/")
 db = client['valenbici']
 collection = db['sample_table']
 
 # API URL para obtener datos de muestra
 api_url = 'https://valencia.opendatasoft.com/api/explore/v2.1/catalog/datasets/valenbisi-disponibilitat-valenbisi-dsiponibilidad/records?limit=20'
 
-def update_data():
-    try:
+# Intervalo de actualización (5 minutos = 300 segundos)
+intervalo_actualizacion = 300
+
+try:
+    while True:
         # Realizar solicitud HTTP a la API
         response = requests.get(api_url)
 
@@ -21,22 +25,23 @@ def update_data():
             # Obtener datos JSON de la respuesta
             data = response.json()
 
-            # Insertar solo registros que no existen en la base de datos
-            for record in data['records']:
-                if collection.count_documents({'_id': record['_id']}) == 0:
-                    collection.insert_one(record)
-                    print(f"Nuevo registro insertado en {time.ctime()}")
+        # Insertar cada documento en MongoDB
+            for entry in data['results']:
+                collection.insert_one(entry)
+
+            # Imprimir mensaje en la consola
+            print("Datos insertados correctamente. Próxima actualización en 5 minutos.")
 
         else:
             print(f"Error al obtener datos: {response.status_code}")
 
-    except Exception as e:
-        print(f"Error: {e}")
+        # Esperar 5 minutos antes de la próxima actualización
+        time.sleep(intervalo_actualizacion)
 
-# Programar la ejecución cada 5 minutos
-schedule.every(5).minutes.do(update_data)
-
-# Ejecutar el programa en un bucle infinito
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+except KeyboardInterrupt:
+    print("\nProceso interrumpido por el usuario.")
+except Exception as e:
+    print(f"Error: {e}")
+finally:
+    # Cerrar la conexión a MongoDB al finalizar el script
+    client.close()
